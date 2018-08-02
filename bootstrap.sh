2b49a6f6c042668a09d200a10c8da30bff0d0c19
Nicole Renée Hubbard <nicole@nicolerenee.io>
2018-08-01 21:27:55 -0600

    Adding flux and bootstrap script. Disabling the metallb since flux seems to have a hard time with it

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl apply -f namespaces/flux.yaml
+kubectl apply -f namespaces/n10-system.yaml
+kubectl apply -f ~/sealed-secrets-key.json
+kubectl apply -f n10-system/kubeseal/
+kubectl apply -f flux/flux-cloud.yaml -f flux/flux-git-deploy.json -f flux/sa.yaml -f flux/memcached.yaml
+kubectl apply -f flux/deployment.json
+kubectl apply -f metallb/metallb.yaml.disabled
